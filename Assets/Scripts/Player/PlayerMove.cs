1cbc8cef532c4b20b83779c3454e656d3f413d5a
Rishabh Pal <rish.09.rp@gmail.com>
2023-06-27 16:40:50 +0530

    LevelGenerateMode

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float moveSpeed = 3.0f;
+    public float leftrightSpeed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,18 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed , Space.World);
+
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > LeveLBoundary.leftSide)
+                transform.Translate(Vector3.left * Time.deltaTime * leftrightSpeed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            if(this.gameObject.transform.position.x < LeveLBoundary.rightSide)
+                transform.Translate(Vector3.left * Time.deltaTime * leftrightSpeed * -1, Space.World);
+        }
     }
 }
